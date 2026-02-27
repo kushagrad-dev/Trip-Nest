347be3ff4f7f18c8246ab6aef7e647e0cf02c2c9
kushagrad-dev <codehack212007@gmail.com>
2026-02-28 02:12:36 +0530

    booking_syste_changes_3

--- models/booking.js
+++ models/booking.js
@@ -6,20 +6,41 @@ const bookingSchema = new mongoose.Schema({
     ref: "Listing",
     required: true
   },
+
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true
   },
-  checkIn: Date,
-  checkOut: Date,
-  guests: Number,
-  totalPrice: Number,
+
+  checkIn: {
+    type: Date,
+    required: true
+  },
+
+  checkOut: {
+    type: Date,
+    required: true
+  },
+
+  guests: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+
+  totalPrice: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
+
 
 // ---------------- CHECK AVAILABILITY STATIC METHOD ----------------
 bookingSchema.statics.isAvailable = async function(listingId, checkIn, checkOut) {
@@ -28,16 +49,29 @@ bookingSchema.statics.isAvailable = async function(listingId, checkIn, checkOut)
     checkIn: { $lt: checkOut },
     checkOut: { $gt: checkIn }
   });
+
   return !conflict;
 };
 
+
 // ---------------- AUTO VALIDATE DATES BEFORE SAVE ----------------
-bookingSchema.pre("save", function(next){
-  if(this.checkOut <= this.checkIn){
+bookingSchema.pre("save", function(next) {
+
+  if (!this.checkIn || !this.checkOut) {
+    return next(new Error("Check-in and Check-out dates required"));
+  }
+
+  if (this.checkOut <= this.checkIn) {
     return next(new Error("Check-out must be after check-in"));
   }
+
+  if (this.checkIn < new Date()) {
+    return next(new Error("Cannot book past dates"));
+  }
+
   next();
 });
+
 
 // ---------------- CALCULATE NIGHTS VIRTUAL ----------------
 bookingSchema.virtual("nights").get(function(){
